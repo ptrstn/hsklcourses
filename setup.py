e3eebef6f8dae27fdf59a8bdfc9b6c4cc7366565
Peter Stein <peterstein@protonmail.com>
2020-04-11 21:13:22 +0200

    Fix Travis CI

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name="hsklcourses",
-    version="0.4.0",
+    version="0.4.1",
     description="Folder structure generator for courses of the University of Applied Sciences Kaiserslautern",
     url="http://github.com/ptrstn/hsklcourses",
     author="Peter Stein",
     license="WTFPL",
     packages=["hsklcourses"],
-    install_requires=["requests", "beautifulsoup4"],
+    install_requires=["requests", "beautifulsoup4", "pandas"],
 )
